6a5e25d1797afde7c247a8c13a6a901ced015a44
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2020-10-12 22:05:05 -0300

    State machine config

---
+++ src/main/java/com/example/demo/domain/enums/BeerOrderStatusEnum.java
@@ -0,0 +1,8 @@
+package com.example.demo.domain.enums;
+
+public enum BeerOrderStatusEnum {
+    NEW, VALIDATED, VALIDATION_EXCEPTION,
+    ALLOCATED, ALLOCATION_EXCEPTION,
+    PENDING_INVENTORY,
+    PICKED_UP, DELIVERED, DELIVERY_EXCEPTION
+}
